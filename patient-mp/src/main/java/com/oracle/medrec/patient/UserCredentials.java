d8c92f42ecca0fedbd2b820715190867a743b1ab
oracle <oracle@integration.oracle.com>
2018-12-12 12:54:24 -0800

    Added all files

---
+++ patient-mp/src/main/java/com/oracle/medrec/patient/UserCredentials.java
@@ -0,0 +1,33 @@
+package com.oracle.medrec.patient;
+
+
+public class UserCredentials {
+    public UserCredentials() {
+        super();
+    }
+    
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public UserCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
